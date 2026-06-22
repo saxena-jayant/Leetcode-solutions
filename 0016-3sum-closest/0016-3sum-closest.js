500cb7ced7e03078dbc1ce4f8c6c2fc8c87a108c
Jayant Saxena <66661729+saxena-jayant@users.noreply.github.com>
2026-06-22 20:41:55 +0530

    Time: 10 ms (94.34%), Space: 57 MB (56.5%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.js
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -4,15 +4,26 @@
  * @return {number}
  */
 var threeSumClosest = function(nums, target) {
-    let result = nums[0] + nums[1] + nums[2];
+    nums.sort((a, b) => a - b);
+
+  let result = nums[0] + nums[1] + nums[2];
 
   for (let i = 0; i < nums.length - 2; i++) {
-    for (let j = i + 1; j < nums.length - 1; j++) {
-      for (let k = j + 1; k < nums.length; k++) {
-        let sum = nums[i] + nums[j] + nums[k];
-        if (Math.abs(sum - target) < Math.abs(result - target)) {
-          result = sum;
-        }
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      const currentSum = nums[i] + nums[left] + nums[right];
+      if (currentSum === target) {
+        return currentSum;
+      }
+      if (Math.abs(currentSum - target) < Math.abs(result - target)) {
+        result = currentSum;
+      }
+      if (currentSum < target) {
+        left++;
+      } else {
+        right--;
       }
     }
   }
