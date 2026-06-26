0b0744da079a80448ccb5795828ca00eb7b4a4d3
Jayant Saxena <66661729+saxena-jayant@users.noreply.github.com>
2026-06-26 14:44:55 +0530

    Time: 1 ms (75.82%), Space: 54 MB (70.21%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    const result = [];
+
+    function backtrack(currentString, openCount, closeCount) {
+        if (currentString.length === n * 2) {
+            result.push(currentString);
+            return;
+        }
+
+        if (openCount < n) {
+            backtrack(currentString + "(", openCount + 1, closeCount);
+        }
+
+        if (closeCount < openCount) {
+            backtrack(currentString + ")", openCount, closeCount + 1);
+        }
+    }
+
+    backtrack("", 0, 0);
+    
+    return result;
+};
